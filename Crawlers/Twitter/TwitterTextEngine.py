6258484fdf17692870bceb111bbd53926033c99d
ef1500 <45581646+ef1500@users.noreply.github.com>
2020-12-18 16:02:47 -0700

    Roughly finished writing the text engine

--- Crawlers/Twitter/TwitterTextEngine.py
+++ Crawlers/Twitter/TwitterTextEngine.py
@@ -45,4 +45,24 @@ def obtainHashtags(filename, user):
                 if match:
                     csvwriter_two.writerow(["Hashtag", htregex.text.encode('utf-8')])
 
-#TODO: NAME ENGINE
+# Step two: Obtain any names that might come up
+# NAME ENGINE SUBPROCESS
+# 1. GET ALL MENTIONS
+# 2. GET ANY NAME FROM THAT USER'S PAGE
+# 3. ADD AS DECNTRALIZED TARGET
+def obtainMentions(filename, dec_targets):
+    Mention_open = open(target_mention_filename, 'a')
+    csvwriter_two = csv.writer(Mention_open)
+    with open(filename, 'r') as ctable:
+        reader = csv.reader(ctable)
+        for row in reader:
+            for field in row:
+                mnregex = (r'(?<=\s|^)@(\w*[A-Za-z_]+\w*)', field)
+                if match:
+                    csvwriter_two.writerow(["Mention", mnregex.text.encode('utf-8')])
+                    tuser = api.get_user(mnregex)
+                    screen_name = tuser.screen_name
+                    dec_open = open(dec_targets, 'a')
+                    csvwriter_three = csv.writer(dec_open)
+                    csvwriter_three.writerow(["Name"], screen_name.encode('utf-8')) #TODO: write to column that corresponds with the source
+                    csvwriter_three.writerow(["Username"], mnregex.encode('utf-8'))
